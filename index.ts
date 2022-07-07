975618063f3d22c63fba02e8ebc2700f8334ca3d
Michael Mathews <michael.mathews@workrise.com>
2022-07-07 11:18:50 +0100

    save

--- index.ts
+++ index.ts
@@ -2,6 +2,8 @@ import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
 
 const SECRET = Deno.env.get("SECRET");
 
+console.log('secret', SECRET);
+
 serve((_req) => {
   return new Response("Hello World 2!" + SECRET, {
     headers: { "content-type": "text/plain" },
