db3378e6233a998eb8a32e3c69ad168acce82d0f
Michael Mathews <michael.mathews@workrise.com>
2022-07-07 12:17:36 +0100

    wip

--- index.ts
+++ index.ts
@@ -4,13 +4,12 @@ import * as postgres from "https://deno.land/x/postgres@v0.14.0/mod.ts";
 const databaseUrl = Deno.env.get("DATABASE_URL")!;
 const pool = new postgres.Pool(databaseUrl, 3, true);
 
-
 serve(async (_req) => {
   const connection = await pool.connect();
-  const result = await connection.queryObject`
-    SELECT * FROM todos
+  const { rows } = await connection.queryObject`
+    SELECT label FROM "TODOS";
   `;
-  return new Response(JSON.stringify(result, {
+  return new Response(JSON.stringify(rows), {
     headers: { "content-type": "text/plain" },
   });
 });
