72f1ef98df0c0783dfd8583d7574644efb1d1064
Michael Mathews <michael.mathews@workrise.com>
2022-07-07 11:47:37 +0100

    wip

--- index.ts
+++ index.ts
@@ -1,11 +1,16 @@
 import { serve } from "https://deno.land/std@0.140.0/http/server.ts";
+import * as postgres from "https://deno.land/x/postgres@v0.14.0/mod.ts";
 
-const SECRET = Deno.env.get("SECRET");
+const databaseUrl = Deno.env.get("DATABASE_URL")!;
+const pool = new postgres.Pool(databaseUrl, 3, true);
 
-console.log('secret', SECRET);
 
-serve((_req) => {
-  return new Response("Hello World 2!" + SECRET, {
+serve(async (_req) => {
+  const connection = await pool.connect();
+  const result = await connection.queryObject`
+    SELECT * FROM todos
+  `;
+  return new Response(JSON.stringify(result, {
     headers: { "content-type": "text/plain" },
   });
 });
